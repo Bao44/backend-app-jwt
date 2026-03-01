450399999bbd89c9399a3a6287558bd402d9aa97
Bao44 <tqbao44@gmail.com>
2026-03-01 15:20:40 +0700

    implement passport

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,20 +6,32 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Request,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './passport/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  // Cách 1: không dùng Guard
+  // @Post('login')
+  // create(@Body() createAuthDto: CreateAuthDto) {
+  //   return this.authService.signIn(
+  //     createAuthDto.username,
+  //     createAuthDto.password,
+  //   );
+  // }
+
+  // Cách 2: dùng Guard
   @Post('login')
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.signIn(
-      createAuthDto.username,
-      createAuthDto.password,
-    );
+  @UseGuards(LocalAuthGuard)
+  async handleLogin(@Request() req) {
+    return this.authService.login(req.user);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,6 +10,28 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.usersService.findByEmail(username);
+
+    const isValidPassword = await comparePasswordHelper(
+      password,
+      user?.password || '',
+    );
+
+    if (!user || !isValidPassword) return null;
+
+    return user;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.email, sub: user._id };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+  // Cách 1: không dùng Guard
   async signIn(username: string, password: string): Promise<any> {
     const user = await this.usersService.findByEmail(username);
 
